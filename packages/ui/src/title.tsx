a66ac12111ae1da2cfa022f4a9521e55c15651d7
Adam Bardsley <adam.bardsley@gmail.com>
2025-02-02 23:35:30 +0000

    Adding a title package used by web only

---
+++ packages/ui/src/title.tsx
@@ -0,0 +1,25 @@
+import { type JSX } from "react";
+
+export function Title({
+  className,
+  children,
+  href,
+}: {
+  className?: string;
+  title: string;
+  children: React.ReactNode;
+  href: string;
+}): JSX.Element {
+  return (
+    <a
+      className={className}
+      href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      <h2>
+      {children} <span> → </span>
+      </h2>
+    </a>
+  );
+}
